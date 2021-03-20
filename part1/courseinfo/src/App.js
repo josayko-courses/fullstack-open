c60985544efa39e450811e3f7cf8214d45a86e8e
Jonny Saykosy <josayko@pm.me>
2021-03-20 13:27:34 +0100

    part 1a done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
 
 const App = () => {
   const course = 'Half Stack application development';
@@ -11,17 +14,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
