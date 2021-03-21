5e0ad4ebe3cd00dfc8ccbcdadbab133a8686da92
Jonny Saykosy <josayko@pm.me>
2021-03-21 16:14:24 +0100

    1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,13 +9,16 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good + bad * -1) / (good + neutral + bad)}</div>
+      <div>positive {(good / (good + neutral + bad)) * 100} %</div>
     </div>
   );
 };
