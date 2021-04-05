cd53a5f0369870caf4b40ae035dba6cd2f17ab28
Jonny Saykosy <josayko@pm.me>
2021-04-05 18:38:13 +0200

    exercise 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNum, setNewNum] = useState('');
+  const [search, setSearch] = useState('');
+  const [numbersList, setNumbersList] = useState(persons);
 
   const isDuplicateName = (persons) => {
     let result = false;
@@ -40,9 +47,25 @@ const App = () => {
     setNewNum(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    let result = persons;
+
+    setSearch(event.target.value);
+    if (event.target.value) {
+      result = persons.filter((person) =>
+        person.name.toLowerCase().includes(event.target.value.toLowerCase())
+      );
+    }
+    setNumbersList(result);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearch} />
+      </div>
+      <h2>Add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -57,8 +80,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.id}>
+      {numbersList.map((person) => (
+        <div key={person.number}>
           {person.name} {person.number}
         </div>
       ))}
