ead94aa8435fb60f19703756500fe7ce92d84d49
Jonny Saykosy <josayko@pm.me>
2021-04-11 17:51:40 +0200

    update 2.13

--- part2/data_for_countries/src/components/Content.js
+++ part2/data_for_countries/src/components/Content.js
@@ -1,25 +1,11 @@
+import CountryInfo from './CountryInfo';
+
 const Content = ({ filteredArray }) => {
   if (filteredArray.length > 10) {
     return <div>Too many matches, specify another filter</div>;
   } else if (filteredArray.length === 1) {
-    return (
-      <>
-        <h2>{filteredArray[0].name}</h2>
-        <div>capital {filteredArray[0].capital}</div>
-        <div>population {filteredArray[0].population}</div>
-        <h3>languages</h3>
-        <ul>
-          {filteredArray[0].languages.map((lang) => {
-            return <li key={lang.iso639_1}>{lang.name}</li>;
-          })}
-        </ul>
-        <img
-          style={{ height: '100px', width: '100px' }}
-          src={filteredArray[0].flag}
-          alt={filteredArray[0].name + ' flag'}
-        />
-      </>
-    );
+    console.log(filteredArray[0]);
+    return <CountryInfo country={filteredArray[0]} />;
   } else {
     return (
       <div>
---
+++ part2/data_for_countries/src/components/CountryInfo.js
@@ -0,0 +1,23 @@
+const CountryInfo = ({ country }) => {
+  console.log(country);
+  return (
+    <>
+      <h2>{country.name}</h2>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map((lang) => {
+          return <li key={lang.iso639_1}>{lang.name}</li>;
+        })}
+      </ul>
+      <img
+        style={{ height: '100px', width: '100px' }}
+        src={country.flag}
+        alt={country.demonym + ' flag'}
+      />
+    </>
+  );
+};
+
+export default CountryInfo;
