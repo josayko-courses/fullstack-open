bc6f93cfd8c557ebd2062757131cbb8babb4deeb
Jonny Saykosy <josayko@pm.me>
2021-04-21 15:11:38 +0200

    3.11 done

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
@@ -87,7 +88,7 @@ app.get('/info', (request, response) => {
   `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- part3/phonebook_frontend/src/services/persons.js
+++ part3/phonebook_frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://fierce-sands-77875.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = async () => {
   const request = axios.get(baseUrl);
