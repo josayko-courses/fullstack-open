a0576464e1c42a4b4e8fff9d5e02cbfdc1263c88
Jonny Saykosy <josayko@pm.me>
2021-04-21 12:56:45 +0200

    TODO: 3.8 update with cors

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] :response-time ms - :body')
 );
+
+app.use(cors());
 
 let persons = [
   {
