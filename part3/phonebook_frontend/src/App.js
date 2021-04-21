4905fe2a35716d2f6b741cc2469d1dc941e9cd6d
Jonny Saykosy <josayko@pm.me>
2021-04-21 13:57:55 +0200

    TODO: 3.8 update baseUrl

--- part3/phonebook_frontend/src/App.js
+++ part3/phonebook_frontend/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     personService.getAll().then((initialList) => {
       setPersons(initialList);
     });
-  }, [persons]);
+  }, []);
 
   const isDuplicateName = (persons) => {
     let result = false;
