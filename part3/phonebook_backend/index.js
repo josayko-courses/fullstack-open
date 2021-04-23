da20c882fb804fea16d769eff4adf228df4e101e
Jonny Saykosy <josayko@pm.me>
2021-04-23 13:09:25 +0200

    3.14 done. TODO: Error handling

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -15,28 +15,7 @@ app.use(
 
 app.use(cors());
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4
-  }
-];
+let persons = [];
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook API</h1>');
@@ -49,13 +28,9 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -74,13 +49,14 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'name must be unique' });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: Math.random()
-  };
-  persons = persons.concat(person);
-  response.json(person);
+    number: body.number
+  });
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.get('/info', (request, response) => {
