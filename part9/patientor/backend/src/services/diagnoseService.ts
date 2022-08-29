7b217652d5aeb66bd4392fc2f4353f628536ad95
Jonny Saykosy <josayko@pm.me>
2022-08-29 17:15:51 +0200

    add diagnoseService. TODO: refactor index.js with router

---
+++ part9/patientor/backend/src/services/diagnoseService.ts
@@ -0,0 +1,14 @@
+import diagnoseData from '../../data/diagnoses.json';
+
+const getDiagnoses = () => {
+  return diagnoseData;
+};
+
+const addDiagnose = () => {
+  return null;
+};
+
+export default {
+  getDiagnoses,
+  addDiagnose
+};
